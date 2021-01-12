c955a9b66aab2691d9c9d3e1245714507024fc8f
el08 <pawliko14@gmail.com>
2021-01-12 07:37:21 +0100

    added GUI, added Observer Pattern added sending/receiving
    what next :
    - try catches, more informations,
    - there is no error message, when COM port is busy - fix it
    - refactor code
    
    * added StringValueClass

---
+++ src/main/java/com/port/serial/StringValueClass.java
@@ -0,0 +1,28 @@
+package com.port.serial;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+
+// peace of Observer Pattern
+public class StringValueClass {
+
+    private static String value ;
+    protected PropertyChangeSupport propertyChangeSupport;
+
+
+    public StringValueClass( ){
+        propertyChangeSupport = new PropertyChangeSupport(this);
+        System.out.println("Created support");
+    }
+
+    public void  setText(String text) {
+        String oldText = this.value;
+        propertyChangeSupport.firePropertyChange("MyTextProperty", oldText,text);
+        System.out.println("value : "+ value);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+}
